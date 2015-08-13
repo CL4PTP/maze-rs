9c3759dbc97bf25c96f4c5577c83692c32738c93
Bence Meszaros <bence.me@gmail.com>
2015-08-12 23:48:36 -0400

    Resolved race condition in the parallel Sidewinder algorithm

--- src/generator/sidewinder_generator.rs
+++ src/generator/sidewinder_generator.rs
@@ -5,7 +5,6 @@ use ::lcg_rng::LCGRng;
 use ::{Grid, Generator, GeneratorOption};
 use ::utils::Direction::{S, E};
 use self::rand::{Rng, SeedableRng};
-use std::convert::Into;
 
 pub struct SidewinderGenerator<'a, G: 'a + Grid + Send + Sync> {
 	grid: &'a mut G,
@@ -65,20 +64,21 @@ impl<'a, G: 'a + Grid + Send + Sync> Generator for SidewinderGenerator<'a, G> {
 			threads.push(thread::scoped(move || {
 				let grid: &mut G = unsafe { transmute(&**grid) };
 
-				for y in (tn * rows_per_thread)..((tn + 1) * rows_per_thread) {
+				for y in ((tn * rows_per_thread)..((tn + 1) * rows_per_thread)).rev() {
 					let mut run_start = 0;
 
 					for x in 0..grid.width() {
-						if y > 0 && (x + 1 == grid.width() || rng.next_f64() > 0.50) {
+						if y < grid.height() - 1
+							&& (x + 1 == grid.width() || rng.next_f64() > 0.50) {
 							let carve_point =
 								run_start + (rng.next_f64() * (x - run_start + 1) as f64) as u64;
 
 							// NOTE: here's something interesting:
 							// if you replace `or_set` with `or_set_unchecked`, it gets slower.
-							grid.or_set(carve_point, y - 1, S.into());
+							grid.or_set(carve_point, y, S as u8);
 							run_start = x + 1;
 						} else if x + 1 < grid.width() {
-							grid.or_set(x, y, E.into());
+							grid.or_set(x, y, E as u8);
 						}
 					}
 				}
